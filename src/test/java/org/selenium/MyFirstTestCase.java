b8b3b1c4cd33d55fdd9742d3fc9bd21fcf6aafc6
murat sayrım <muratsayrim@hotmail.com.tr>
2022-03-15 01:52:13 +0300

    first

---
+++ src/test/java/org/selenium/MyFirstTestCase.java
@@ -0,0 +1,57 @@
+package org.selenium;
+
+import org.openqa.selenium.By;
+import org.selenium.pom.base.BaseTest;
+import org.selenium.pom.pages.HomePage;
+import org.selenium.pom.pages.StorePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyFirstTestCase extends BaseTest {
+
+    @Test
+    public void guestCheckoutUsingDricetBankTransfer() throws InterruptedException {
+        driver.get("https://www.askomdch.com");
+        HomePage homePage = new HomePage(driver);
+        StorePage storePage = homePage.clickStoreMenuLink();
+        storePage.
+                enterTextInSearchFld("Blue").
+                clickSearchBtn();
+        Assert.assertEquals(storePage.getTitle(),"Search results: “Blue”");
+        storePage.clickAddToCartBtn();
+
+        driver.findElement(By.cssSelector("#menu-item-1227 > a")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("woocommerce-product-search-field-0")).sendKeys("Blue");
+        driver.findElement(By.cssSelector("button[value='Search']")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector(".woocommerce-products-header__title.page-title")).getText(),
+                "Search results: “Blue”"
+        );
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("a[aria-label='Add “Blue Shoes” to your cart']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("a[title='View cart']")).click();
+        Assert.assertEquals(
+                driver.findElement(By.cssSelector("td[class='product-name'] a")).getText(),
+                "Blue Shoes"
+        );
+        driver.findElement(By.cssSelector(".checkout-button")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("billing_first_name")).sendKeys("demo");
+        driver.findElement(By.id("billing_last_name")).sendKeys("user");
+        driver.findElement(By.id("billing_address_1")).sendKeys("San Francisco");
+        driver.findElement(By.id("billing_city")).sendKeys("San Francisco");
+        driver.findElement(By.id("billing_postcode")).sendKeys("94188");
+        driver.findElement(By.id("billing_email")).sendKeys("askomdch@gmail.com");
+        Thread.sleep(3000);
+        driver.findElement(By.id("place_order")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(
+                By.cssSelector(".woocommerce-notice")).getText(),
+                "Thank you. Your order has been received."
+        );
+
+    }
+}
